306c455fc342a67b9f5b08a2c1545c6157c284f7
andersemmanuel <andersemmanuel93@gmail.com>
2025-05-12 16:21:27 +0200

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les routes</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <?php
+         include_once DIR .'/routes/routes.php';
+    ?>
+
+    <main>
+       <!-- <?php include_once DIR .'/pages/$page.php' ?> -->
+    </main>
+
+    <footer>
+        <p>&copy; - PHP -2025</php>
+    </footer>
+</body>
+</html>
